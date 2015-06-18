707082e80c67ffb5d5550cc2857d982471163fbc
ferreol <ferreol@ioamac56.epfl.ch>
2015-06-18 15:47:37 +0200

    Adding Fresnel and finite difference operator

---
+++ LinOp/Fresnel.m
@@ -0,0 +1,115 @@
+classdef Fresnel <  LinOp
+    %% Fresnel : Discrete Fresnel operator
+    %  Matlab Linear Operator Library
+    %
+    % Obj = Fresnel(lambda, n0, z,dxy,sz, pad)
+    % Fresnel transform operator
+    %
+    %
+    % Please refer to the LINOP superclass for general documentation about
+    % linear operators class
+    % see also LinOp
+    
+    %     Copyright (C) 2015 F. Soulez ferreol.soulez@epfl.ch
+    %
+    %     This program is free software: you can redistribute it and/or modify
+    %     it under the terms of the GNU General Public License as published by
+    %     the Free Software Foundation, either version 3 of the License, or
+    %     (at your option) any later version.
+    %
+    %     This program is distributed in the hope that it will be useful,
+    %     but WITHOUT ANY WARRANTY; without even the implied warranty of
+    %     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    %     GNU General Public License for more details.
+    %
+    %     You should have received a copy of the GNU General Public License
+    %     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    properties (SetAccess = protected,GetAccess = public)
+        lambda  % wavelenght            [m]
+        n0      % refractive index of the medium
+        k0      % wavenumber in vaccum  [m-1]
+        k       % wavenumber in the medium [m-1]
+        z       % depth of propagation  [m]
+        dxy     % pixel size            [m]
+        Nx      % number of pixels along X
+        Ny      % number of pixels along Y
+        pad     % size with padding
+        u       % u frequency grid
+        v       % v frequency grid
+        Fu      % Fresnel function along the u axis
+        Fv      % Fresnel function along the v axis
+        F       % Fresnel function
+        FeitFleck = false; % if true use the Feit and Fleck model of propagation instead: 
+        % M. D. Feit and J. A. Fleck, ?Bean nonparaxiality, filament formaform, and beam breakup in the self-focusing of optical beams,? J. Opt. Soc. Am. B, vol. 5, pp. 633? 640, March 1988. 
+    end
+    methods
+        function this = Fresnel(lambda, n0, z,dxy,sz, pad, varargin)
+            
+            this.name ='Fresnel';
+            this.iscomplex= true;
+            this.isinvertible=true;
+            
+            assert(isPositiveScalar(lambda),'The wavelenght lambda should be a positive scalar');
+            this.lambda = lambda;
+            
+            assert(isPositiveScalar(n0),'The refractive index n0 should be a positive scalar');
+            this.n0 = n0;
+            
+            assert(isscalar(z),'The propagation depth z should be a scalar');
+            this.z = z;
+            
+            assert(isPositiveScalar(dxy),'The pixel size dxy should be a positive scalar');
+            this.dxy = dxy;
+            
+            assert(issize(sz) && (length(sz)~=2),'The input size sz should be a conformable  to size(2D) ');
+            this.sizein = sz;
+            this.sizeout = sz;
+            this.Nx = sz(1);
+            this.Ny = sz(2);
+            
+            assert((~isempty(pad))&& issize(pad)&& length(pad)~=2,'The padding pad should be a conformable  to size(2D)');
+            this.pad = pad;
+            
+            for c=1:length(varargin)
+            switch varargin{c}
+                case('FeitFleck')
+                    this.FeitFleck = true;
+            end
+            end
+            
+            this.k0 = 2*pi/this.lambda;
+            this.k = this.n0*this.k0;
+            
+            this.u = 1./(this.Nx *this.dxy) * [0:this.Nx/2-1, -this.Nx/2:-1];
+            this.v = 1./(this.Ny *this.dxy) * [0:this.Ny/2-1, -this.Ny/2:-1]';
+            
+            this.Fu = exp(-1i* pi *  this.z.* this.lambda / this.n0 * this.u.^2);
+            this.Fv = exp(-1i* pi *  this.z.* this.lambda / this.n0 * this.v.^2);
+            
+            if this.FeitFleck
+                Mesh = kron(this.u.^2, this.v.^2);
+                this.F =  exp(-2i* pi * this.lambda * Mesh ./ (1 + sqrt(1 + (this.lambda/this.n0)^2 *Mesh)));
+            else
+            this.F = kron(this.Fu, this.Fv);
+            end
+            
+        end
+        function y = Apply(this,x)
+            assert( isequal(size(x),this.sizein),  'x does not have the right size: [%d, %d]',this.sizein);
+            y = ifft2( this.F .*  fft2(x));
+        end
+        function y = Adjoint(this,x)
+            assert( isequal(size(x),this.sizeout),  'x does not have the right size: [%d, %d]',this.sizeout);
+            y = ifft2(  conj(this.F) .*  fft2(x));
+        end
+        function y = Inverse(this,x)
+            assert( isequal(size(x),this.sizeout),  'x does not have the right size: [%d, %d]',this.sizeout);
+            y = x;
+        end
+        function y = AdjointInverse(this,x)
+            assert( isequal(size(x),this.sizein),  'x does not have the right size: [%d, %d]',this.sizein);
+            y = x;
+        end
+    end
+end
---
+++ LinOp/Grad.m
@@ -0,0 +1,172 @@
+classdef Grad <  LinOp
+    %% Grad :  Finite difference operator
+    %  Matlab Linear Operator Library
+    %
+    % Obj = Grad(sz,index)
+    % Finite operator operator
+    % Compute finite differences of a vector of size SZ along the dimension
+    % indexed in INDEX (all by default)
+    % The output is then of size SZ x lenght(index)
+    % To 
+    %
+    % Please refer to the LINOP superclass for general documentation about
+    % linear operators class
+    % see also LinOp
+    
+    %     Copyright (C) 2015 F. Soulez ferreol.soulez@epfl.ch
+    %
+    %     This program is free software: you can redistribute it and/or modify
+    %     it under the terms of the GNU General Public License as published by
+    %     the Free Software Foundation, either version 3 of the License, or
+    %     (at your option) any later version.
+    %
+    %     This program is distributed in the hope that it will be useful,
+    %     but WITHOUT ANY WARRANTY; without even the implied warranty of
+    %     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    %     GNU General Public License for more details.
+    %
+    %     You should have received a copy of the GNU General Public License
+    %     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    properties (SetAccess = protected,GetAccess = public)
+        index % index along wich dimension are computed the finite differences
+        lgthidx % length of INDEX
+        ndms  % number of dimension of the input
+    end
+    methods
+        function this = Grad(sz,index)
+            if nargin == 1
+                index = [];
+            end
+             this.name ='Grad';
+            this.iscomplex= false;
+            this.isinvertible=false;
+            
+            
+            assert(issize(sz),'The input size sz should be a conformable  to a size ');
+            this.sizein = sz;
+            this.ndms = length(this.sizein);
+            % Special case for vectors as matlab thought it is matrix ;-( 
+            if this.sizein(2) ==1
+                this.ndms = 1;
+            end
+            
+            if (~isempty(index))
+                assert(isvector(index) && length(index)<= this.ndms && max(index)<= this.ndms,'The index should be a conformable  to sz');
+                this.index = index;
+            else
+                this.index = 1:this.ndms;
+            end
+            this.lgthidx = length(this.index);
+            %size of the output = size of the input x length of the index
+            % Special case for vectors as matlab thought it is matrix ;-( 
+            if this.sizein(2) ==1
+                 this.sizeout= [this.sizein(1),this.lgthidx];                
+            else
+                 this.sizeout= [this.sizein,this.lgthidx];
+            end
+        end
+        function y = Apply(this,x)
+            assert( isequal(size(x),this.sizein),  'x does not have the right size: [%d, %d, %d,%d]',this.sizein);
+            y = zeros(this.sizeout);
+            nidx = 0;
+            % switch according to the number of dimension of the input
+            switch(this.ndms)
+                % 1 dimension
+                case(1)
+                    y(:,1) = padarray(x(1:end-1,1) - x(2:end,1),1,'post');
+                % 2 dimensions
+                case(2)
+                    for n=this.index
+                        nidx = nidx +1;
+                        switch(n)
+                            case(1)
+                                y(:,:,nidx) = padarray(x(1:end-1,:)- x(2:end,:),1,'post');
+                            case(2)
+                                y(:,:,nidx) = padarray(x(:,1:end-1)- x(:,2:end),[0 1],'post');
+                        end
+                    end
+                % 3 dimensions
+                case(3)
+                    for n=this.index
+                        nidx = nidx +1;
+                        switch(n)
+                            case(1)
+                                y(:,:,:,nidx) = padarray(x(1:end-1,:,:)- x(2:end,:,:),1,'post');
+                            case(2)
+                                y(:,:,:,nidx) = padarray(x(:,1:end-1,:)- x(:,2:end,:),[0 1],'post');
+                            case(3)
+                                y(:,:,:,nidx) = padarray(x(:,:,1:end-1)- x(:,:,2:end),[0 0 1],'post');
+                        end
+                    end
+                % 4 dimensions
+                case(4) 
+                    for n=this.index
+                        nidx = nidx +1;
+                        switch(n)
+                            case(1)
+                                y(:,:,:,:,nidx) = padarray(x(1:end-1,:,:,:)- x(2:end,:,:,:),1,'post');
+                            case(2)
+                                y(:,:,:,:,nidx) = padarray(x(:,1:end-1,:,:)- x(:,2:end,:,:),[0 1],'post');
+                            case(3)
+                                y(:,:,:,:,nidx) = padarray(x(:,:,1:end-1,:)- x(:,:,2:end,:),[0 0 1],'post');
+                            case(4)
+                                y(:,:,:,:,nidx) = padarray(x(:,:,:,1:end-1)- x(:,:,:,2:end),[0 0 0 1],'post');
+                        end
+                    end
+            end
+        end
+        function y = Adjoint(this,x)
+            assert( isequal(size(x),this.sizeout),  'x does not have the right size: [%d, %d, %d,%d,%d]',this.sizeout);
+            nidx = 0;
+             y = zeros(this.sizein);
+            % switch according to the number of dimension of the input
+            switch(this.ndms)
+                % 1 dimension
+                case(1)
+                    y =  padarray(  x(1:end-1),1,'post') - padarray( x(1:end-1),1,'pre');
+                % 2 dimensions
+                case(2)
+                    for n=this.index
+                        nidx = nidx +1;
+                        switch(n)
+                            case(1)
+                                y = y + padarray(  x(1:end-1,:,nidx),1,'post') - padarray( x(1:end-1,:,nidx),1,'pre');
+                            case(2)
+                                y = y + padarray(  x(:,1:end-1,nidx),[0 1],'post') - padarray( x(:,1:end-1,nidx),[0 1],'pre');
+                        end
+                    end
+                % 3 dimensions
+                case(3)
+                    for n=this.index
+                        nidx = nidx +1;
+                        switch(n)
+                            case(1)
+                                y = y + padarray(  x(1:end-1,:,:,nidx),1,'post') - padarray( x(1:end-1,:,:,nidx),1,'pre');
+                            case(2)
+                                y = y + padarray(  x(:,1:end-1,:,nidx),[0 1],'post') - padarray( x(:,1:end-1,:,nidx),[0 1],'pre');
+                            case(3)
+                                y = y + padarray(  x(:,:,1:end-1,nidx),[0  0 1],'post') - padarray( x(:,:,1:end-1,nidx),[0 0 1],'pre');
+                        end
+                    end
+                % 4 dimensions
+                case(4) 
+                    for n=this.index
+                        nidx = nidx +1;
+                        switch(n)
+                            case(1)
+                                y = y + padarray(  x(1:end-1,:,:,:,nidx),1,'post') - padarray( x(1:end-1,:,:,:,nidx),1,'pre');
+                            case(2)
+                                y = y + padarray(  x(:,1:end-1,:,:,nidx),[0 1],'post') - padarray( x(:,1:end-1,:,:,nidx),[0 1],'pre');
+                            case(3)
+                                y = y + padarray(  x(:,:,1:end-1,:,nidx),[0  0 1],'post') - padarray( x(:,:,1:end-1,:,nidx),[0 0 1],'pre');
+                            case(4)
+                                y = y + padarray(  x(:,:,:,1:end-1,nidx),[0  0 0 1],'post') - padarray( x(:,:,:,1:end-1,nidx),[0 0 0 1],'pre');
+                        end
+                    end
+            end
+        
+            
+        end
+    end
+end

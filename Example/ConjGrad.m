196a0e260ce1f0c5aa9c32c9f220c108322b7069
ferreol <ferreol.soulez@epfl.ch>
2015-07-03 14:59:08 +0200

    Add conjugate gradient function in example folder

---
+++ Example/ConjGrad.m
@@ -0,0 +1,52 @@
+%% Conjugate gradient 
+%
+% Example 
+% x = ConjGrad(A,b,  x0,maxiter)
+% Estimate the solution of the eqution A*x = b with XO the initilization
+% and maxiter the maximum number of iteration 
+
+%     Copyright (C) 2015 F. Soulez ferreol.soulez@epfl.ch
+%
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or (at
+%     your option) any later version.
+%
+%     This program is distributed in the hope that it will be useful, but
+%     WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%     General Public License for more details.
+%
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+function x = ConjGrad(A,b,  x0,maxiter)
+x = x0;% starting point
+r = b - A*x;
+k = 0;
+while k< maxiter
+    rho = dot(r(:), r(:));
+    if k == 0
+        tol = epsilon*rho;
+        p = r;
+    elseif rho <= tol
+        break
+    else
+        beta = rho/rho_prec;
+        p = r + beta*p;
+    end
+    q = A*p;
+    alpha = rho/dot(p(:), q(:));
+    x = x + alpha*p;
+    r = r - alpha*q;
+    rho_prec = rho;
+    k = k + 1;
+end
+
+%------------------------------------------------------------------------------
+% Local Variables:
+% mode: Matlab
+% tab-width: 8
+% fill-column: 78
+% coding: utf-8
+% End:

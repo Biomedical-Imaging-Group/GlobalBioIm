2b85c64fd1115c71ee39221ad7749131d07b0227
Mike McCann (BIG desktop) <tokkot@gmail.com>
2016-12-02 14:55:11 +0100

    adding steepest descent optimization method

---
+++ Optimization/steepest.m
@@ -0,0 +1,31 @@
+function x = steepest(A, b, x, iMax ,W)
+% steepest descent method from https://www.cs.cmu.edu/~quake-papers/painless-conjugate-gradient.pdf
+% appendix B1
+
+HtH = false;
+if (nargin>4) && (~isempty(W))
+    HtH = true;
+end
+if HtH
+    r = b - A.HtWH(x,W);
+else
+    r = b - A*x;
+end
+
+delta = r(:)'*r(:);
+delta0 = delta;
+for i = 1:iMax
+	if delta <= eps*delta0
+		break;
+	end
+	if HtH
+		q = A.HtWH(r,W);
+	else
+		q = A*r;
+	end
+	alpha = delta/(r(:)'*q(:));
+	x = x + alpha * r;
+	r = r - alpha * q;
+	delta = r(:)'*r(:);
+	
+end

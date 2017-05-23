c1e2021901e0bb7e11b12e609703142b3ef90dfe
Thomas Debarre <thomasdebarre@epfl.ch>
2017-05-23 10:37:08 +0200

    changed back what I thought was a bug

--- LinOp/OperationsOnLinOp/SumLinOp.m
+++ LinOp/OperationsOnLinOp/SumLinOp.m
@@ -74,8 +74,8 @@ classdef SumLinOp < LinOp
 			this.ALinOp = ALinOp;
 			this.iscomplex= this.ALinOp{1}(1).iscomplex;
             this.isinvertible=false;
-            this.sizein = max(this.ALinOp{1}(1).sizein);
-            this.sizeout = max(this.ALinOp{1}(1).sizeout);
+            this.sizein = this.ALinOp{1}(1).sizein;
+            this.sizeout = this.ALinOp{1}(1).sizeout;
             for n =2:this.numLinOp
                 assert(isempty(this.ALinOp{n}(1).sizein)  || isequal(this.sizein,this.ALinOp{n}(1).sizein),'%d-th input does not have the right hand side size ', n) ;
                 assert(isempty(this.ALinOp{n}(1).sizeout) ||isequal(this.sizeout,this.ALinOp{n}(1).sizeout),'%d-th input does not have the left hand side size ', n);

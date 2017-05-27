e2f1fc87c19a2ac2a57e2fd23021dc0f8620f80c
Emmanuel Soubies <emmanuel@vpn-253-230.epfl.ch>
2017-05-27 13:14:39 +0200

    displaying functions in Utils/: orthoviews + modifs in imdisp

--- Util/imdisp.m
+++ Util/imdisp.m
@@ -1,16 +1,20 @@
-function imdisp(im,tilt,newfig)
-%% function imdisp(im,tilt)
+function imdisp(im,titl,newfig)
+%% function imdisp(im,titl,newfig)
 %
 % Display a 2D image with gray colormap, removing the grid 
 % and keeping the proportions of the image when displaying
-% newfig is a boolean true to create a new figure
+% - im is the image to display
+% - titl is the title of the figure
+% - newfig is a boolean true to create a new figure (default true)
+%
 % -- Example
-% im=imread('cameraman.tif')
-% imdisp(im,'Input image',1);
+% im=imread('cameraman.tif');
+% imdisp(im,'Cameraman',1);
 %
 % Copyright (C) 2017 E. Soubies emmanuel.soubies@epfl.ch
 
+if nargin ==2, newfig=1; end;
 if newfig, figure; end
 imagesc(im); colormap gray; axis off; axis image;
-title(tilt);
+title(titl);set(gca,'FontSize',14);
 end

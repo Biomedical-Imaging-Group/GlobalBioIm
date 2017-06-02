076155f90886a92fcc8086299802edac54bd154e
Emmanuel Soubies <emmanuel@ioamac36.epfl.ch>
2017-06-02 12:21:22 +0200

    Changes in Orthoview utility

--- Util/Orthoviews.m
+++ Util/Orthoviews.m
@@ -1,11 +1,12 @@
-function Orthoviews(im,pos,newfig)
-%% function Orthoviews(im,pos,newfig)
+function Orthoviews(im,pos,tt,newfig)
+%% function Orthoviews(im,pos,tt,newfig)
 %
 % Display a orthoviews of a 3D image with gray colormap, removing the grid 
 % and keeping the proportions of the image when displaying. 
 % - im is the image to display
 % - pos is a vector containing the position to extract planes in (i,j,k)
 %   coordinates (i.e. (row,colunm,frame))
+% - tt is for a global tilte
 % - newfig is a boolean true to create a new figure (default true)
 %
 % -- Example
@@ -18,7 +19,8 @@ function Orthoviews(im,pos,newfig)
 sz=size(im);im=double(im)/double(max(im(:)));
 margin=10;
 if nargin <2, pos=floor(sz/2); end;
-if nargin <3, newfig=1; end;
+if nargin <3, tt=' '; end;
+if nargin <4, newfig=1; end;
 if newfig, figure; end
 assert(length(sz)==3,'Wrong length of pos vector');
 assert(all(pos>0) && all(pos<=sz),'Given position out of image range');
@@ -34,8 +36,12 @@ line([sz(2)+margin+1 sz(2)+margin+sz(3)+1],[pos(1) pos(1)],'Color','b','LineWidt
 line([1 sz(2)],[sz(1)+margin+pos(3) sz(1)+margin+pos(3)],'Color','b','LineWidth',2);
 line([pos(2) pos(2)],[sz(1)+margin+1 sz(1)+margin+sz(3)],'Color','b','LineWidth',2);
 text(-5,pos(1),['I=',num2str(pos(1))],'FontSize',14,'HorizontalAlignment','right')
-text(pos(2),-5,['J=',num2str(pos(2))],'FontSize',14,'HorizontalAlignment','center')
-text(sz(2)+margin+pos(3),-5,['K=',num2str(pos(3))],'FontSize',14,'HorizontalAlignment','center')
+text(pos(2),-7,['J=',num2str(pos(2))],'FontSize',14,'HorizontalAlignment','center')
+text(sz(2)+margin+pos(3),-7,['K=',num2str(pos(3))],'FontSize',14,'HorizontalAlignment','center')
 text(-5,sz(1)+margin+pos(3),['K=',num2str(pos(3))],'FontSize',14,'HorizontalAlignment','right')
+
+ax=axes('Units','Normal','Position',[.075 .075 .85 .85],'Visible','off');
+set(get(ax,'Title'),'Visible','on','FontSize',14)
+title(tt);
 
 end

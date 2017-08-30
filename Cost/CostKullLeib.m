1feceadd38d7539318c6ec264fdbb945ab72cb9c
Emmanuel Soubies <emmanuel@tsf-436-wpa-7-042.epfl.ch>
2017-08-30 15:03:40 +0200

    now all 2D deconvolution examples are working

--- Cost/CostKullLeib.m
+++ Cost/CostKullLeib.m
@@ -1,6 +1,6 @@
 classdef CostKullLeib < Cost
-    % KullbackLeibler divergence
-    % $$ C(\\mathrm{x}) :=\\sum_n D_{KL}((\\mathrm{Hx})_n)$$
+    % CostKullLeib: KullbackLeibler divergence
+    % $$ C(\\mathrm{x}) :=\\sum_n D_{KL}(\\mathrm{x}_n)$$
     % where
     % $$ D_{KL}(\\mathrm{z}_n) := \\left\\lbrace \\begin{array}[ll]
     % \\mathrm{z}_n - \\mathrm{y}_n \\log(\\mathrm{z}_n + \\beta) & \\text{ if } \\mathrm{z}_n + \\beta >0  \\newline
@@ -11,9 +11,12 @@ classdef CostKullLeib < Cost
     %
     % :param bet: smoothing parameter \\(\\beta\\) (default 0) 
     %
+    % **Example** C=CostKullLeib(sz,y,bet)
+    %
     % See also :class:`Cost` :class:`LinOp`
 
-    %     Copyright (C) 2017 E. Soubies emmanuel.soubies@epfl.ch
+    %%    Copyright (C) 2017 
+    %     E. Soubies emmanuel.soubies@epfl.ch
     %
     %     This program is free software: you can redistribute it and/or modify
     %     it under the terms of the GNU General Public License as published by
@@ -32,79 +35,60 @@ classdef CostKullLeib < Cost
     properties (SetAccess = protected,GetAccess = public)
         bet= 0;     % smoothing parameter, if bet=0 then the unsmoothed version is used
     end
-    % Full protected properties
-    properties (SetAccess = protected,GetAccess = protected)
-        He1;     % Application of the adjoint of H to a vector of ones
-        Hx;     % H.apply(x)
-    end
     
-    methods
-        %% Constructor
-        function this = CostKullLeib(H,y,bet)
-            this.name='Cost Kullback-Leibler';
-            this.isconvex=true;
-            % -- Set entries
-            if nargin<2
-                y=0;
-            end
-            if nargin<1
-                H=[];
-            end
-            set_y(this,y);
-            set_H(this,H);
-            
-            if nargin==3
-                this.bet=bet;
-            end
-            tmp=ones(this.H.sizeout);
-            this.He1=this.H.applyAdjoint(tmp);
-            
-            % -- Compute Lipschitz constant of the gradient (if the norm of H is known)
-            if ((this.bet>0)&&(this.H.norm>=0));
-                this.lip=max(this.y(:))./this.bet^2*this.H.norm^2;
+    %% Constructor
+    methods       
+        function this = CostKullLeib(sz,y,bet)
+            if nargin<2, y=0; end
+            this@Cost(sz,y);
+            this.name='CostKullLeib';        
+            if nargin==3, this.bet=bet;end
+            this.isConvex=true;            
+            % -- Compute Lipschitz constant of the gradient
+            if (this.bet>0)
+                this.lip=max(this.y(:))./this.bet^2;
+                this.isDifferentiable=true;
             end
         end
-
-        function f=eval(this,x)
+    end
+    
+    %% Core Methods containing implementations (Protected)
+    % - apply_(this,x)
+    % - applyGrad_(this,x)
+    % - applyProx_(this,x,alpha)
+	methods (Access = protected)
+        function f=apply_(this,x)
         	% Reimplemented from parent class :class:`Cost`.
         	
-            this.Hx=this.H.apply(x);
-            if ~any(this.Hx(:)<0)
+            if ~any(x(:)<0)
                 f=Inf;
             else
                 if (this.bet~=0)
-                    f=sum(-this.y(:).*log(this.Hx(:)+this.bet) + this.Hx(:));
+                    f=sum(-this.y(:).*log(x(:)+this.bet) + x(:));
                 else
-                    ft = zeros(size(this.Hx));
-                    zidx = (this.Hx~=0);
-                    ft(zidx)=-this.y(zidx).*log(this.Hx(zidx)) + this.Hx(zidx);
+                    ft = zeros(this.sz);
+                    zidx = (x~=0);
+                    ft(zidx)=-this.y(zidx).*log(x(zidx)) + x(zidx);
                     f=sum(ft(:));
                 end
             end
         end
-
-        function g=grad(this,x)
+        function g=applyGrad_(this,x)
         	% Reimplemented from parent class :class:`Cost`.
-        	
-            if nargin ==2
-            this.Hx=this.H.apply(x);
+            
+            g= 1 - this.y./(x+this.bet);
+        end  
+        function z=applyProx_(this,x,alpha)
+            % Reimplemented from parent class :class:`Cost`.
+            
+            if (this.bet~=0)
+                delta=(x-alpha-this.bet).^2+4*(x*this.bet + alpha*(this.y-this.bet));
+                z=zeros(size(x));
+                mask=delta>=0;
+                z(mask)=0.5*(x(mask)-alpha-this.bet + sqrt(delta(mask)));
+            else
+                z=applyProx_@Cost(this,x,alpha);
             end
-            g= this.He1 - this.H.applyAdjoint(this.y./(this.Hx+this.bet));
-        end
-        
-        function z=prox(this,x,alpha)
-        	% Reimplemented from parent class :class:`Cost`.
-        	
-            z=[];
-            if isa(this.H,'LinOpIdentity') % if operator identity
-                if (this.bet~=0)
-                    delta=(x-alpha-this.bet).^2+4*(x*this.bet + alpha*(this.y-this.bet));
-                    z=zeros(size(x));
-                    mask=delta>=0;
-                    z(mask)=0.5*(x(mask)-alpha-this.bet + sqrt(delta(mask)));
-                end
-            end
-            if isempty(z),error('Prox not implemented');end
         end
     end
 end
--- Opti/OptiRichLucy.m
+++ Opti/OptiRichLucy.m
@@ -3,7 +3,8 @@ classdef OptiRichLucy < Opti
     % divergence :class:`CostKullLeib` (with TV regularization [3]).
     % $$ C(\\mathrm{x})= F(\\mathrm{x}) + \\lambda \\Vert \\mathrm{x} \\Vert_{\\mathrm{TV}} $$
     %
-    % :param F: :class:`CostKullLeib` object
+    % :param F: :class:`CostKullLeib` object or a :class:`CostComposition`
+    % with a :class:`CostKullLeib` and a :class:`LinOp`
     % :param TV: boolean true if TV regularization used  (default false)
     % :param lambda: regularization parameter (when TV used)
     % :param epsl: smoothing parameter to make TV differentiable at 0 (default \\(10^{-6}\\))
@@ -24,10 +25,13 @@ classdef OptiRichLucy < Opti
     % [3] N. Dey et al. "Richardson-Lucy Algorithm With Total Variation Regularization for 3D Confocal Microscope 
     % Deconvolution." Microscopy research and technique (2006).
     %
+    % **Example** RL=OptiRichLucy(F,TV,lamb,OutOp)
+    %
     % See also :class:`Opti`, :class:`OutputOpti`, :class:`Cost`,
     % :class:`CostKullLeib`
     
-    %     Copyright (C) 2017 E. Soubies emmanuel.soubies@epfl.ch
+    %%    Copyright (C) 2017 
+    %     E. Soubies emmanuel.soubies@epfl.ch
     %
     %     This program is free software: you can redistribute it and/or modify
     %     it under the terms of the GNU General Public License as published by
@@ -54,27 +58,33 @@ classdef OptiRichLucy < Opti
     % Full protected properties
     properties (SetAccess = protected,GetAccess = protected)
 		G;   % gradient operator (used if TV activated)
-		Fkl; % Kullback-Leibler divergence
+		F;   % Kullback-Leibler divergence
+        isH; % boolean true if F is a CostComposition
     end
    
     methods
     	%% Constructor
     	function this=OptiRichLucy(F,TV,lamb,OutOp)
     		this.name='Opti Richardson-Lucy';
-    		assert(isa(F,'CostKullLeib'), 'The minimized functional should be the FuncKullLeib');
-    		this.cost=F;
+    		assert((isa(F,'CostComposition') && isa(F.H1,'CostKullLeib') && isa(F.H2,'LinOp') ) || ...
+                isa(F,'CostKullLeib'), 'The minimized functional should be the FuncKullLeib or a CostComposition between a CostKullLeib and a LinOp');
+            if ~isa(F,'CostComposition')
+                this.F=F*LinOpDiag(F.sizein);
+            else
+                this.F=F;
+            end
     		if nargin==4 && ~isempty(OutOp)
     			this.OutOp=OutOp;
     		end
-    		if nargin>=2 && ~isempty(TV), this.TV=TV; end
-    		this.Fkl=F;
+    		if nargin>=2 && ~isempty(TV), this.TV=TV; end    		
+            this.cost=this.F;
     		if nargin>=3 && ~isempty(lamb), this.lamb=lamb; end
     		if this.TV
-    			this.G=LinOpGrad(this.Fkl.H.sizein);
-    			if length(this.Fkl.H.sizein)==2  % 2D
-    				this.cost=this.cost + this.lamb*CostMixNorm12([3],this.G);
-    			elseif length(this.Fkl.H.sizein)==3
-    				this.cost=this.cost + this.lamb*CostMixNorm12([4],this.G);
+    			this.G=LinOpGrad(this.F.sizein);
+    			if length(this.F.sizein)==2  % 2D
+    				this.cost=this.cost + this.lamb*CostMixNorm21([this.F.sizein,2],3)*this.G;
+    			elseif length(this.F.sizein)==3
+    				this.cost=this.cost + this.lamb*CostMixNorm21([this.F.sizein,3],4)*this.G;
     			end
     		end
     	end 
@@ -83,10 +93,10 @@ classdef OptiRichLucy < Opti
             % Reimplementation from :class:`Opti`.
             
 			if ~isempty(x0),this.xopt=x0;end;  % To restart from current state if wanted
-			assert(~isempty(this.xopt),'Missing starting point x0');
-        	data=this.Fkl.y;
-        	He1=this.Fkl.H.adjoint(ones(this.Fkl.H.sizein));
-        	bet=this.Fkl.bet;
+            assert(~isempty(this.xopt),'Missing starting point x0');
+            data=this.F.H1.y;
+            He1=this.F.H2.applyAdjoint(ones(this.F.sizein));
+            bet=this.F.H1.bet;
             if bet==0, error('Smoothing parameter beta has to be different from 0 (see constructor of CostKullLeib)'); end;
 			tstart=tic;
 			this.OutOp.init();
@@ -95,23 +105,23 @@ classdef OptiRichLucy < Opti
 			while (this.niter<this.maxiter)
 				this.niter=this.niter+1;
 				xold=this.xopt;
-				% - Algorithm iteration
-				if ~this.TV
-					this.xopt=this.xopt./He1.*this.Fkl.H.adjoint(data./(this.Fkl.H.apply(this.xopt)+bet));	
-				else
-					tmp=this.G.apply(this.xopt);
-					if length(size(tmp))==2     % 1D
-						nor=sqrt(tmp.^2+this.epsl);
+                % - Algorithm iteration
+                if ~this.TV
+                    this.xopt=this.xopt./He1.*this.F.H2.applyAdjoint(data./(this.F.H2.apply(this.xopt)+bet));
+                else
+                    tmp=this.G.apply(this.xopt);
+                    if length(size(tmp))==2     % 1D
+                        nor=sqrt(tmp.^2+this.epsl);
 					elseif length(size(tmp))==3 % 2D
 						nor=repmat(sqrt(sum(tmp.^2,3)+this.epsl),[1,1,size(tmp,3)]);
-					elseif length(size(tmp))==4 % 3D
-						nor=repmat(sqrt(sum(tmp.^2,4)+this.epsl),[1,1,1,size(tmp,4)]);
-					end
-					gradReg=this.G.adjoint(tmp./nor);
-					this.xopt=this.xopt./(He1 + this.lamb*gradReg).*this.Fkl.H.adjoint(data./(this.Fkl.H.apply(this.xopt)+bet));
-					if sum(this.xopt(:)<0)~=0
-  						warning('Violation of the positivity of the solution (the regularization parameter should be decreased).');
-  					end
+                    elseif length(size(tmp))==4 % 3D
+                        nor=repmat(sqrt(sum(tmp.^2,4)+this.epsl),[1,1,1,size(tmp,4)]);
+                    end
+                    gradReg=this.G.applyAdjoint(tmp./nor);
+                    this.xopt=this.xopt./(He1 + this.lamb*gradReg).*this.F.H2.applyAdjoint(data./(this.F.H2.apply(this.xopt)+bet));
+                    if sum(this.xopt(:)<0)~=0
+                        warning('Violation of the positivity of the solution (the regularization parameter should be decreased).');
+                    end
 				end		
 				% - Convergence test
 				if this.test_convergence(xold), break; end

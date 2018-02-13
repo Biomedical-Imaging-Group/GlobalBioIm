60022134785d921222cc5a5fcfee953dced32881
ferreol <ferreol.soulez@free.fr>
2018-02-13 14:48:05 +0100

    Add positivity constraint in CostL1

--- Cost/CostL1.m
+++ Cost/CostL1.m
@@ -2,13 +2,17 @@ classdef CostL1 < Cost
     % CostL1: L1 norm cost function
     % $$C(x) := \\|\\mathrm{x} - \\mathrm{y}\\|_1 $$
     %
-    % All attributes of parent class :class:`Cost` are inherited. 
+    % If nonneg is set to true, it add positivity constraint on x:
+    % $$C(x) := \\|\\mathrm{x} - \\mathrm{y}\\|_1 + i(\\mathrm{x} - \\mathrm{y}) $$
+    % where i() is the indicator function of $\\mathbb{R}^{+}
     %
-    % **Example** C=CostL1(sz,y)
+    % All attributes of parent class :class:`Cost` are inherited.
+    %
+    % **Example** C=CostL1(sz,y,nonneg)
     %
     % See also :class:`Map` :class:`Cost`, :class:`LinOp`
-
-    %%    Copyright (C) 2015 
+    
+    %%    Copyright (C) 2015
     %     F. Soulez ferreol.soulez@epfl.ch
     %
     %     This program is free software: you can redistribute it and/or modify
@@ -23,39 +27,64 @@ classdef CostL1 < Cost
     %
     %     You should have received a copy of the GNU General Public License
     %     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+    
     %% Constructor
+    properties
+        nonneg = false;
+    end
     methods
-        function this = CostL1(sz,y)
-            if nargin<2, y=0; end
+        function this = CostL1(sz,y,nonneg)
+            if nargin<2
+                y=0;
+                nonneg=false;
+            else if nargin<3
+                    nonneg=false;
+                else
+                    nonneg = nonneg;
+                end
+            end
             this@Cost(sz,y);
             this.name='CostL1';
             this.isConvex=true;
-            this.isDifferentiable=false;      
+            this.isDifferentiable=false;
+            this.nonneg = nonneg;
         end
     end
-
+    
     %% Core Methods containing implementations (Protected)
     % - apply_(this,x)
     % - applyProx_(this,x,alpha)
-	methods (Access = protected)
-        function y=applyProx_(this,x,alpha) 
+    methods (Access = protected)
+        function y=applyProx_(this,x,alpha)
             % Reimplemented from parent class :class:`Cost`.
             % $$ \\mathrm{prox}_{\\alpha C}(\\mathrm{x}) = \\mathrm{sign(x-y)} \\mathrm{max}(\\vert x-y \\vert- \\alpha,0)+  \\mathrm{y} $$
-            if(isscalar(this.y)&&(this.y==0))
-                y =  sign(x) .* max( abs( x) - alpha,0);
+            if this.nonneg
+                if(isscalar(this.y)&&(this.y==0))
+                    y =    max( x - alpha,0);
+                else
+                    tmp = x-this.y ;
+                    y =    max( tmp - alpha,0)+this.y;
+                end
             else
-                tmp = x-this.y ;
-                y =  sign(tmp) .* max( abs( tmp) - alpha,0)+this.y;
+                if(isscalar(this.y)&&(this.y==0))
+                    y =  sign(x) .* max( abs( x) - alpha,0);
+                else
+                    tmp = x-this.y ;
+                    y =  sign(tmp) .* max( abs( tmp) - alpha,0)+this.y;
+                end
             end
         end
         function y=apply_(this,x)
             % Reimplemented from parent class :class:`Cost`.
+              if this.nonneg && any(x(:)<0)
+              y = + inf;
+              else
             if(isscalar(this.y)&&(this.y==0))
                 y=sum(abs(x(:)));
             else
                 y=sum(abs(x(:)-this.y(:)));
             end
+              end
         end
     end
 end

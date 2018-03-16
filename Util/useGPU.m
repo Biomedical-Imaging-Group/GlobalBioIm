4b3a56fef719a9e1802fc6adbe8f01ad279a49af
Emmanuel Soubies <soubies@ioamac53.epfl.ch>
2018-03-16 21:23:30 +0100

    modif in useGPU function

--- Util/useGPU.m
+++ Util/useGPU.m
@@ -6,19 +6,29 @@ function useGPU(v)
 %   v=1 : uses the parrallel computing toolbox of Matlab (gpuArray) [1]
 %   v=2 : uses the CudaMat Library [2]
 %
-% [1]
-% [2]
+% [1] https://ch.mathworks.com/help/distcomp/gpu-computing-in-matlab.html
+% [2] https://nanoimaging.de/research/software/cudamat/#Related_Software
 %
 % Copyright (C) 2018 E. Soubies emmanuel.soubies@epfl.ch
 %--------------------------------------------------------------
 
-global isGPU cuda_enabled; isGPU=v;
-if isGPU==2
+global isInitCuda isGPU cuda_enabled gpuDev; isGPU=v;
+if  ~isempty(cuda_enabled) && isGPU~=2
+    disableCuda();
+    cuda_enabled=[];
+end
+if ~isempty(gpuDev) && isGPU~=1
+    reset(gpuDev);
+    gpuDev=[];
+end
+if isGPU==1 && isempty(gpuDev)
+    gpuDev=gpuDevice();
+elseif isGPU==2 && isempty(cuda_enabled)
     assert(exist('initCuda')>0,'useGPU(2) requires CudaMat which seems to be not installed or not on Matlab path. See https://nanoimaging.de/research/software/cudamat/');
-    initCuda();enableCuda();
-else
-    if ~isempty(cuda_enabled)
-        disableCuda();
+    if isempty(isInitCuda)
+        isInitCuda=1;
+        initCuda();
     end
+    enableCuda();
 end
 end

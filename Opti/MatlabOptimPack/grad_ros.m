750ed2fb92ebd7895c0d82cebaa79c7c250d9e4c
Ferreol <ferreol.soulez@univ-lyon1.fr>
2018-07-25 18:49:20 +0200

    Few fixes in v1.1 release

--- Opti/MatlabOptimPack/grad_ros.m
+++
@@ -1,7 +0,0 @@
-function g = grad_ros(x);
-% Compute the gradient of the rosenbrock function
-
-g = zeros_(size(x));
-g(1) = -2.0*(1.0-x(1)) - 400.0*(x(2)-x(1).^2)*x(1);
-g(2) = 200.0*(x(2)-x(1).^2);
- 

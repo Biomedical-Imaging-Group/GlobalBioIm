5385857ee5446f877f4c2939ecd34974b7dc0318
Emmanuel Soubies <emmanuel@tsf-436-wpa-2-056.epfl.ch>
2018-08-08 11:07:48 +0200

    add new unitary tests

--- LinOp/Tests/TestsSummationLinOps.m
+++ LinOp/Tests/TestsSummationLinOps.m
@@ -7,6 +7,8 @@ H=LinOpConv(rand(sz));
 x=rand(sz);
 fprintf('\nOperators used : \n \t - D : LinOpDiag  \n \t - H : LinOpConv \n \n');
 tol=1e-13;
+isOk=true;
+me=[];
 
 %% Tests
 fprintf('Test #1 : \n ');
@@ -17,8 +19,9 @@ fprintf(['\t D+H+D            --> ',T.name,': ',mapCell{:},'\n']);
 fprintf( '\t                      Check if (D+H+D)x = Dx+Hx+Dx')
 if norm(T*x-(D*x+H*x+D*x)) < tol
     fprintf('  --> OK \n');
-else
-    fprintf('  --> FAILED \n');
+else   
+    fprintf('  --> FAILED \n');isOk=false;
+    me=sprintf('%s %s \n \t ',me,' (D+H+D)x ~= Dx+Hx+Dx ');
 end
 T=D+D+H;
 mapCell=cellfun(@(x) x.name,T.mapsCell,'UniformOutput',false);
@@ -28,7 +31,8 @@ fprintf( '\t                      Check if (D+D+H)x = Dx+Hx+Dx')
 if norm(T*x-(D*x+H*x+D*x)) < tol
     fprintf('  --> OK \n');
 else
-    fprintf('  --> FAILED \n');
+    fprintf('  --> FAILED \n');isOk=false;
+    me=sprintf('%s %s \n \t ',me,' (D+D+H)x ~= Dx+Hx+Dx ');
 end
 T=H+D+D;
 mapCell=cellfun(@(x) x.name,T.mapsCell,'UniformOutput',false);
@@ -38,7 +42,8 @@ fprintf( '\t                      Check if (H+D+D)x = Dx+Hx+Dx')
 if norm(T*x-(D*x+H*x+D*x)) < tol
     fprintf('  --> OK \n');
 else
-    fprintf('  --> FAILED \n');
+    fprintf('  --> FAILED \n');isOk=false;
+    me=sprintf('%s %s \n \t ',me,' (H+D+D)x ~= Dx+Hx+Dx ');
 end
 T=D+(D+H);
 mapCell=cellfun(@(x) x.name,T.mapsCell,'UniformOutput',false);
@@ -48,7 +53,8 @@ fprintf( '\t                      Check if (D+(D+H))x = Dx+Hx+Dx')
 if norm(T*x-(D*x+H*x+D*x)) < tol
     fprintf('  --> OK \n');
 else
-    fprintf('  --> FAILED \n');
+    fprintf('  --> FAILED \n');isOk=false;
+    me=sprintf('%s %s \n \t ',me,' (D+(D+H))x ~= Dx+Hx+Dx ');
 end
 T=D+H+H+D;
 mapCell=cellfun(@(x) x.name,T.mapsCell,'UniformOutput',false);
@@ -58,7 +64,8 @@ fprintf( '\t                      Check if (D+H+H+D)x = Dx+Hx+Hx+Dx')
 if norm(T*x-(D*x+H*x+D*x+H*x)) < tol
     fprintf('  --> OK \n');
 else
-    fprintf('  --> FAILED \n');
+    fprintf('  --> FAILED \n');isOk=false;
+    me=sprintf('%s %s \n \t ',me,' (D+H+H+D)x ~= Dx+Hx+Hx+Dx ');
 end
 T=(D+H)+(H+D);
 mapCell=cellfun(@(x) x.name,T.mapsCell,'UniformOutput',false);
@@ -68,7 +75,8 @@ fprintf( '\t                      Check if ((D+H)+(H+D))x = Dx+Hx+Hx+Dx')
 if norm(T*x-(D*x+H*x+D*x+H*x)) < tol
     fprintf('  --> OK \n');
 else
-    fprintf('  --> FAILED \n');
+    fprintf('  --> FAILED \n');isOk=false;
+    me=sprintf('%s %s \n \t ',me,' ((D+H)+(H+D))x ~= Dx+Hx+Hx+Dx ');
 end
 T=D-(H+0.5*D);
 mapCell=cellfun(@(x) x.name,T.mapsCell,'UniformOutput',false);
@@ -78,7 +86,8 @@ fprintf( '\t                      Check if (D-(H+0.5*D))x = Dx-Hx-0.5*Dx')
 if norm(T*x-(D*x-H*x-0.5*D*x)) < tol
     fprintf('  --> OK \n');
 else
-    fprintf('  --> FAILED \n');
+    fprintf('  --> FAILED \n');isOk=false;
+    me=sprintf('%s %s \n \t ',me,' (D-(H+0.5*D))x ~= Dx-Hx-0.5*Dx ');
 end
 T=D+H-0.5*D;
 mapCell=cellfun(@(x) x.name,T.mapsCell,'UniformOutput',false);
@@ -88,7 +97,8 @@ fprintf( '\t                      Check if (D+H-0.5*D)x = Dx+Hx-0.5*Dx')
 if norm(T*x-(D*x+H*x-0.5*D*x)) < tol
     fprintf('  --> OK \n');
 else
-    fprintf('  --> FAILED \n');
+    fprintf('  --> FAILED \n');isOk=false;
+    me=sprintf('%s %s \n \t ',me,' (D+H-0.5*D)x ~= Dx+Hx-0.5*Dx ');
 end
 T=(D+H)-0.5*(H+D);
 mapCell=cellfun(@(x) x.name,T.mapsCell,'UniformOutput',false);
@@ -98,5 +108,12 @@ fprintf( '\t                      Check if ((D+H)-0.5*(H+D))x = Dx+Hx-0.5*(Hx+Dx
 if norm(T*x-(D*x+H*x-0.5*(D*x+H*x))) < tol
     fprintf('  --> OK \n');
 else
-    fprintf('  --> FAILED \n');
+    fprintf('  --> FAILED \n');isOk=false;
+    me=sprintf('%s %s \n \t ',me,' ((D+H)-0.5*(H+D))x ~= Dx+Hx-0.5*(Hx+Dx) ');
+end
+
+global generateDataUnitTests stateTest message
+if ~isempty(generateDataUnitTests)
+    stateTest=isOk;
+    message=me;
 end

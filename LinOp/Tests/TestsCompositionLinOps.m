3f6a8ac5e149fbe5795c77441c72cd51d048396a
Emmanuel Soubies <emmanuel@tsf-436-wpa-0-190.epfl.ch>
2018-09-14 15:35:51 +0200

    update TestsCompositionLinOps.m

--- LinOp/Tests/TestsCompositionLinOps.m
+++ LinOp/Tests/TestsCompositionLinOps.m
@@ -13,35 +13,45 @@ me=[];
 %% Test #1 (Scalar multiplication)
 fprintf('Test #1 (Scalar multiplication) : \n ');
 fprintf('\t 2*S*S\''  =2I ');
-if isequal(2*S*S',2*I64)
+Op1=2*S*S';Op1.clearListenerList();
+Op2=2*I64;Op2.clearListenerList();
+if isequal(Op1,Op2)
     fprintf('   ---> OK \n');
 else
     fprintf('   ---> FAILED \n');isOk=false;
     me=sprintf('%s %s \n \t ',me,' 2*S*S\''  ~= 2I');
 end
 fprintf('\t 2*(S*S\'')=2I ');
-if isequal(2*(S*S'),2*I64)
+Op1=2*(S*S');Op1.clearListenerList();
+Op2=2*I64;Op2.clearListenerList();
+if isequal(Op1,Op2)
     fprintf('   ---> OK \n');
 else
     fprintf('   ---> FAILED \n');isOk=false;
     me=sprintf('%s %s \n \t ',me,' 2*(S*S\'') ~= 2I');
 end
 fprintf('\t S*(2*S\'')=2I ');
-if isequal(S*(2*S'),2*I64)
+Op1=S*(2*S');Op1.clearListenerList();
+Op2=2*I64;Op2.clearListenerList();
+if isequal(Op1,Op2)
     fprintf('   ---> OK \n');
 else
     fprintf('   ---> FAILED\n');isOk=false;
     me=sprintf('%s %s \n \t ',me,' S*(2*S\'') ~= 2I');
 end
 fprintf('\t 4*(2*S)*S\''=8I ');
-if isequal(4*(2*S)*S',8*I64)
+Op1=4*(2*S)*S';Op1.clearListenerList();
+Op2=8*I64;Op2.clearListenerList();
+if isequal(Op1,Op2)
     fprintf(' ---> OK \n');
 else
     fprintf(' ---> FAILED\n');isOk=false;
     me=sprintf('%s %s \n \t ',me,' 4*(2*S)*S\'' ~= 8I');
 end
 fprintf('\t 4*S*(2*S\'')=8I ');
-if isequal(4*S*(2*S'),8*I64)
+Op1=4*S*(2*S');Op1.clearListenerList();
+Op2=8*I64;Op2.clearListenerList();
+if isequal(Op1,Op2)
     fprintf(' ---> OK \n');
 else
     fprintf(' ---> FAILED\n');isOk=false;
@@ -51,35 +61,45 @@ end
 %% Test #2 (More complex compositions)
 fprintf('\nTest #2 (More complex compositions) : \n ');
 fprintf('\t S\''*(S*S\'')=S\'' ');
-if isequal(S'*(S*S'),S')
+Op1=S'*(S*S');Op1.clearListenerList();
+Op2=S';Op2.clearListenerList();
+if isequal(Op1,Op2)
     fprintf('                    ---> OK \n');
 else
     fprintf('                    ---> FAILED\n');isOk=false;
     me=sprintf('%s %s \n \t ',me,' S\''*(S*S\'') ~= S\'' ');
 end
 fprintf('\t (H\''*S)*(S\''*H)=H\''*H ');
-if isequal((H'*S)*(S'*H),H'*H)
+Op1=(H'*S)*(S'*H);Op1.clearListenerList();
+Op2=H'*H;Op2.clearListenerList();
+if isequal(Op1,Op2)
     fprintf('              ---> OK \n');
 else
     fprintf('              ---> FAILED\n');isOk=false;
     me=sprintf('%s %s \n \t ',me,' (H\''*S)*(S\''*H) ~= H\''*H ');
 end
 fprintf('\t H\''*S*S\''*H=H\''*H ');
-if isequal(H'*S*S'*H,H'*H)
+Op1=H'*S*S'*H;Op1.clearListenerList();
+Op2=H'*H;Op2.clearListenerList();
+if isequal(Op1,Op2)
     fprintf('                  ---> OK \n');
 else
     fprintf('                  ---> FAILED\n');isOk=false;
     me=sprintf('%s %s \n \t ',me,' H\''*S*S\''*H ~= H\''*H ');
 end
 fprintf('\t 2*H\''*(2*S)*(3*S\'')*(1*H)=12*H\''*H ');
-if isequal(2*H'*(2*S)*(3*S')*(1*H),12*H'*H)
+Op1=2*H'*(2*S)*(3*S')*(1*H);Op1.clearListenerList();
+Op2=12*H'*H;Op2.clearListenerList();
+if isequal(Op1,Op2)
     fprintf(' ---> OK \n');
 else
     fprintf(' ---> FAILED\n');isOk=false;
     me=sprintf('%s %s \n \t ',me,' 2*H\''*(2*S)*(3*S\'')*(1*H) ~= 12*H\''*H ');
 end
 fprintf('\t H\''*(S*S\'')*H=H\''*H ');
-if isequal(H'*(S*S')*H,H'*H)
+Op1=H'*(S*S')*H;Op1.clearListenerList();
+Op2=H'*H;Op2.clearListenerList();
+if isequal(Op1,Op2)
     fprintf('                ---> OK \n');
 else
     fprintf('                ---> FAILED\n');isOk=false;

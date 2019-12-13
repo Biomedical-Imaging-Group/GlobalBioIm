42c86a02563ba883ba63901f302ef65a72d0ac67
E. Soubies <emmanuel.soubies@irit.fr>
2019-12-13 17:32:10 +0100

    make OptiRichLucy work is He1 contains some zeros + better management of warnings

--- Opti/OptiRichLucy.m
+++ Opti/OptiRichLucy.m
@@ -99,9 +99,10 @@ classdef OptiRichLucy < Opti
             % Reimplementation from :class:`Opti`.
             
             initialize@Opti(this,x0);
+            if ~any(this.xopt(:)), this.xopt=ones(size(this.xopt)); end
             this.data=this.F.H1.y;
-            this.He1=this.F.H2.applyAdjoint(ones_(this.F.sizein));
             this.bet=this.F.H1.bet;
+            this.He1=max(this.F.H2.applyAdjoint(ones_(this.F.H2.sizeout)),this.bet);
             if this.bet==0, error('Smoothing parameter beta has to be different from 0 (see constructor of CostKullLeib)'); end;
         end
         function flag=doIteration(this)
@@ -121,7 +122,9 @@ classdef OptiRichLucy < Opti
                 gradReg=this.G.applyAdjoint(tmp./nor);
                 this.xopt=this.xopt./(this.He1 + this.lamb*gradReg).*this.F.H2.applyAdjoint(this.data./(this.F.H2.apply(this.xopt)+this.bet));
                 if sum(this.xopt(:)<0)~=0
+                    warnStruct = warning('off','backtrace');
                     warning('Violation of the positivity of the solution (the regularization parameter should be decreased).');
+                    warning(warnStruct);
                 end
             end
             flag=this.OPTI_NEXT_IT;

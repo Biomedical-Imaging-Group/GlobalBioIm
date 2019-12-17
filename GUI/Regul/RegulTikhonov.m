edf8a9329817e43368420063f438d1c7c5b2323c
E. Soubies <emmanuel.soubies@irit.fr>
2019-12-17 15:42:12 +0100

    fix typo in Regul Tikhonov

--- GUI/Regul/RegulTikhonov.m
+++ GUI/Regul/RegulTikhonov.m
@@ -1,7 +1,7 @@
 %% GUI-Header
 % GUInotation-Tikh-
 % GUIcallOpReg-LinOpGrad(InputSize,index,BC,res)-
-% GUIparam-Index-vecInt-[]-Dimensions along which the gradient is computed (all by default)
+% GUIparam-index-vecInt-[]-Dimensions along which the gradient is computed (all by default)
 % GUIparam-BC-dropDown/circular/zeros/mirror-circular-Boundary condition (default 'circular'), 'zeros', 'mirror'
 % GUIparam-res-vecInt-[]-Vector containing the resolution along each dimension (default all 1)
 % GUIcallCostReg-CostL2(OpReg.sizeout,Weight)-

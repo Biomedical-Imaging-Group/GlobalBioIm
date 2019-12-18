57d9da97fdf4fc1b475b57412f98f4221d3b6d08
E. Soubies <emmanuel.soubies@irit.fr>
2019-12-18 15:52:59 +0100

    add tif reader and writer

--- GUI/GlobalBioIm_Deconvolution_Script.m
+++ GUI/GlobalBioIm_Deconvolution_Script.m
@@ -19,9 +19,13 @@ H2_InputSize = [300 300];
 tmp=load("/home/esoubies/Bureau/GitHub/GlobalBioIm/GUI/psf.mat"); fd=fields(tmp);
 H2_PSF = tmp.(fd{1});
 % - Data-fidelity : L2
-DF_y = double(imread("/home/esoubies/Bureau/GitHub/GlobalBioIm/GUI/data.png"));
-% - Algorithm ADMM-NoFullSplit
-Opt_rho = 1;
+DF_y = double(loadtiff("/home/esoubies/Bureau/GitHub/GlobalBioIm/GUI/data.tif"));
+% - Regularization-1 : Smooth-Total-Variation
+R1_lambda = 0.3;
+% - Algorithm Fwd-Bkwd Splitting
+Opt_gam = 1;
+Opt_fista = 1;
+Opt_updateGam = 'backtracking';
 Opt_TolCost = 1e-4;
 Opt_TolStep = 1e-4;
 % - Path to save results
@@ -40,19 +44,25 @@ H2 = LinOpConv('PSF',H2_PSF,1,[],'Centered','Pad',H2_InputSize,0);
 %% Instanciate the Cost function
 % - Data-Fidelity : L2
 DF = CostL2(H1.sizeout,DF_y);
+% - Regularization-1 : Smooth-Total-Variation
+OpReg1 = LinOpGrad(H2.sizein);
+CostReg1 = R1_lambda * CostHyperBolic(OpReg1.sizeout,[],length(OpReg1.sizeout));
 
 %% Instanciate and Run the Optimization method
-% - Algorithm ADMM-NoFullSplit
-F0 = DF*(H1*H2);
-Fn = {}; Fn = [Fn,{CostNonNeg(F0.sizein)}];
-Hn = {}; Hn = [Hn,{LinOpIdentity(F0.sizein)}];
-Opt = OptiADMM(F0,Fn,Hn,Opt_rho);
-Opt.OutOp = OutputOpti(1,round(Opt.maxiter/10),[1]);
+% - Algorithm Fwd-Bkwd Splitting
+F = DF*(H1*H2) + CostReg1*(OpReg1);
+P = CostNonNeg(F.sizein);
+Opt = OptiFBS(F,P);
+Opt.gam = Opt_gam;
+Opt.fista = Opt_fista;
+Opt.updateGam = Opt_updateGam;
+Opt.OutOp = OutputOpti(1,round(Opt.maxiter/10));
 Opt.ItUpOut = round(Opt.maxiter/10);
 Opt.CvOp = TestCvgCombine('CostRelative',Opt_TolCost, 'StepRelative',Opt_TolStep);
 Opt.run(zeros(Opt.cost.sizein));
 
 %% Display and Save Results
+imdisp(Opt.xopt,['Deconvolution-Result'],1);
 save([resultPath,'_OptiCell'],'Opt');
-imwrite(uint8(Opt.xopt/max(Opt.xopt(:))*255),[resultPath,'.png']);
+saveastiff(single(Opt.xopt),[resultPath,'.tif']);
 
